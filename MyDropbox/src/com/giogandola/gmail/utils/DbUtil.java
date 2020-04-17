af27af8481e74020816b51507680468efbcde042
giogandola98 <giogandola@gmail.com>
2020-04-17 12:50:59 +0200

    change db connection to other pool for heroku deploy

--- MyDropbox/src/com/giogandola/gmail/utils/DbUtil.java
+++ MyDropbox/src/com/giogandola/gmail/utils/DbUtil.java
@@ -1,36 +1,43 @@
 package com.giogandola.gmail.utils;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DbUtil 
 {
-	static DataSource dataSource=null;
+	static BasicDataSource connectionPool=null;
 	private static void getMapping()
 	{
+		URI dbUri;
 		try {
-			// Get DataSource
-			Context initContext  = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			dataSource= (DataSource)envContext.lookup("jdbc/testdb");
+			dbUri = new URI("mysql://bbd6032aafd0b8:95fe8fa0@eu-cdbr-west-02.cleardb.net/heroku_6fca6f9bee22f9c");
+		
+	  String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+	  connectionPool = new BasicDataSource();
 
-			
-		} catch (NamingException e) {
+	  if (dbUri.getUserInfo() != null) {
+	    connectionPool.setUsername(dbUri.getUserInfo().split(":")[0]);
+	    connectionPool.setPassword(dbUri.getUserInfo().split(":")[1]);
+	  }
+	  connectionPool.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	  connectionPool.setUrl(dbUrl);
+	  connectionPool.setInitialSize(1);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
 	public static Connection getConnection()
 	{
-		if(dataSource == null)
+		if(connectionPool == null)
 			getMapping();
 		try {
-			return dataSource.getConnection();
+			return connectionPool.getConnection();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
